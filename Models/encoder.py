9d3331e500863898c70f4d2d79a0492ef42bbade
Zhiyuan <liz27@miamioh.edu>
2022-12-26 18:26:56 -0500

    Update encoder.py

--- Models/encoder.py
+++ Models/encoder.py
@@ -8,8 +8,16 @@ from .multi_head_attention import MultiHeadAttention
 from .utils import clones
 from typing import Union
 
+class FeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1):
+        super(FeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
 
-
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+    
 class LayerNorm(nn.Module):
     def __init__(self, features: int, eps: float = 1e-6):
         # features = d_model
